08b5ba134a1f23c51732cf54179c0169eab8d4fe
Adam Hellberg <sharparam@sharparam.com>
2017-08-08 23:29:16 +0200

    Implement solutions for VI

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -19,12 +19,29 @@ fun task41(): Nothing = TODO(
         }
 )
 
+//fun <T: Collection<E>, E> partitionTo(matching: T, nonmatching: T, predicate: (E) -> Boolean): Pair<T, T> {
+//
+//}
+
+fun <C: MutableCollection<E>, E> Collection<E>.partitionTo(
+        matching: C,
+        nonmatching: C,
+        predicate: (E) -> Boolean
+): Pair<C, C> {
+    forEach {
+        if (predicate(it))
+            matching.add(it)
+        else
+            nonmatching.add(it)
+    }
+
+    return Pair(matching, nonmatching)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
